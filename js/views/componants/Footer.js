8492498d420c2ee04a9647023fdb574aaafcf663
renaudin clement <cronisosdev@gmail.com>
2025-03-19 15:03:07 +0100

    footer

--- js/views/componants/Footer.js
+++ js/views/componants/Footer.js
@@ -1,5 +1,11 @@
 export default Footer = `
 <footer>
+    <ul>
+        <li> <a href="https://example.com">Home</a> </li>
+        <li> <a href="https://example.com">Preset</a> </li>
+        <li> <a href="https://example.com">Compte</a> </li>
+    </ul>
+    
     <p>Footer</p>
 </footer>
 `;
