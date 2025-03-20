47ba404b76e8d5c6758ffc8d478488df314573c3
glados-creator <79933806+glados-creator@users.noreply.github.com>
2025-03-20 07:25:17 +0000

    race list

--- js/model/personnage_cg.js
+++ js/model/personnage_cg.js
@@ -1,6 +1,18 @@
 export default class Personnage_cg {
+    static race_list = [
+        "Altmer",
+        "Argonian",
+        "Bosmer",
+        "Breton",
+        "Dunmer",
+        "Imperial",
+        "Khajiit",
+        "Nord",
+        "Orsimer",
+        "Redguard",]
+
     static #race;
     constructor() {
-        
+
     }
 }
