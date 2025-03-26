ee27b28723b377ecece9e7a1fa2c03a90c0a97c3
Renaudin clement <cronisosdev@gmail.com>
2025-03-27 00:22:56 +0100

    tres grande avancer du js et css

--- js/views/componants/Page_comp.js
+++ js/views/componants/Page_comp.js
@@ -6,13 +6,19 @@ export default class Page_comp {
     static async renderPage(contentCallback) {;
         return `
           ${await HeaderBar.render()}
+
+          
           <div class="background-image"></div>
+          <img src="img/Skyrim-logo.png" alt="Background" class="title-image">
           <div class="main">
-          <section>
           <img src="img/scroll.png" alt="Background" class="main-image">
+          <section class="overlay">
+          <img src="img/skyrim-logo-graphic-xa101sn0p941hozw.png" height="42" width="42" alt="Background" class="logo">
+          ${await contentCallback()}
           </section>
+          <div class = "cache"> </div>
           <section class="gray-div"></section>
-            ${await contentCallback()}
+            
           </div>
           ${await Footer.render()}
         `;
