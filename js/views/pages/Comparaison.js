58bc2b1098d1a7538475bd1a6a37ca4a121c621c
Renaudin clement <cronisosdev@gmail.com>
2025-04-03 08:00:53 +0200

    avancement

--- js/views/pages/Comparaison.js
+++ js/views/pages/Comparaison.js
@@ -14,23 +14,23 @@ export default class Comparaison_page extends Page_comp {
             <h1>Comparaison</h1>
             <p>Page de comparaison</p>
             <div class="equipment-selectors">
-                <div>Helmet: ${Selector_Comp(["helmet"]).render()}</div>
-                <div>Chestpiece: ${Selector_Comp(["chestpiece"]).render()}</div>
-                <div>Pants: ${Selector_Comp(["pants"]).render()}</div>
-                <div>Boots: ${Selector_Comp(["boots"]).render()}</div>
-                <div>Gloves: ${Selector_Comp(["gloves"]).render()}</div>
-                <div>Necklace: ${Selector_Comp(["necklace"]).render()}</div>
-                <div>Ring: ${Selector_Comp(["ring"]).render()}</div>
+                <div>Helmet: ${new Selector_Comp(["helmet"]).render()}</div>
+                <div>Chestpiece: ${new Selector_Comp(["chestpiece"]).render()}</div>
+                <div>Pants: ${new Selector_Comp(["pants"]).render()}</div>
+                <div>Boots: ${new Selector_Comp(["boots"]).render()}</div>
+                <div>Gloves: ${new Selector_Comp(["gloves"]).render()}</div>
+                <div>Necklace: ${new Selector_Comp(["necklace"]).render()}</div>
+                <div>Ring: ${new Selector_Comp(["ring"]).render()}</div>
             </div>
             <div class="character-views">
-                <div>${perso1 ? PersonnageViewComp(perso1).render() : '<button>+</button>'}</div>
-                <div>${perso2 ? PersonnageViewComp(perso2).render() : '<button>+</button>'}</div>
+                <div>${perso1 ? new PersonnageViewComp(perso1).render() : '<button>+</button>'}</div>
+                <div>${perso2 ? new PersonnageViewComp(perso2).render() : '<button>+</button>'}</div>
             </div>
             <table>
                 <tr>
                     <th>Weapons</th>
-                    <td>${Selector_Comp(["Weapons"]).render(perso1)}</td>
-                    <td>${Selector_Comp(["Weapons"]).render(perso2)}</td>
+                    <td>${new Selector_Comp(["Weapons"]).render(perso1)}</td>
+                    <td>${new Selector_Comp(["Weapons"]).render(perso2)}</td>
                 </tr>
             </table>
             <div>${Calculator_comp(perso1, perso2).render()}</div>
